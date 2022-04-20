a1122ae31ad46f7f7436f3604c1b8c7fa21cf550
Sumit K <synergy2411@outlook.com>
2022-04-20 15:17:27 +0530

    form added

--- 02-ts-frontend/build/app.js
+++ 02-ts-frontend/build/app.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Loaded!!!");
+window.onload = () => {
+    const btnAdd = document.getElementById("btnAdd");
+    const txtTitle = document.getElementById("txtTitle");
+    btnAdd.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("Title : ", txtTitle.value);
+    });
+};
--- 02-ts-frontend/src/app.ts
+++ 02-ts-frontend/src/app.ts
@@ -1,1 +1,9 @@
-console.log("Loaded!!!")
+window.onload = () => {
+    const btnAdd = document.getElementById("btnAdd") as HTMLButtonElement;
+    const txtTitle = document.getElementById("txtTitle") as HTMLInputElement;
+
+    btnAdd.addEventListener("click", (event : Event) => {
+        event.preventDefault();
+        console.log("Title : ", txtTitle.value)
+    })
+}
