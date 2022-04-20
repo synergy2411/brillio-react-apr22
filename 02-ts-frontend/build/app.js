6a5530ae3f643f7f59e1132d2403bc18ffab558c
Sumit K <synergy2411@outlook.com>
2022-04-20 15:50:53 +0530

    classes created

--- 02-ts-frontend/build/app.js
+++ 02-ts-frontend/build/app.js
@@ -1,9 +1,40 @@
 "use strict";
+class Payment {
+    constructor(money) {
+        this.money = money;
+    }
+    format() {
+        return `Spend $${this.money.amount} on ${this.money.title.toUpperCase()} dated ${this.money.createdAt}`;
+    }
+}
+class Receipt {
+    constructor(money) {
+        this.money = money;
+    }
+    format() {
+        return `Received $${this.money.amount} for ${this.money.title.toUpperCase()} dated ${this.money.createdAt}`;
+    }
+}
 window.onload = () => {
     const btnAdd = document.getElementById("btnAdd");
     const txtTitle = document.getElementById("txtTitle");
+    const txtCreatedAt = document.getElementById("created-at");
+    const selType = document.querySelector("#sel-type");
+    const txtAmount = document.getElementById("txtAmount");
     btnAdd.addEventListener("click", (event) => {
         event.preventDefault();
-        console.log("Title : ", txtTitle.value);
+        let money = {
+            amount: parseInt(txtAmount.value),
+            createdAt: new Date(txtCreatedAt.value),
+            title: txtTitle.value
+        };
+        let doc;
+        if (selType.value === "payment") {
+            doc = new Payment(money);
+        }
+        else {
+            doc = new Receipt(money);
+        }
+        console.log(doc);
     });
 };
--- 02-ts-frontend/src/app.ts
+++ 02-ts-frontend/src/app.ts
@@ -1,9 +1,52 @@
+
+interface MoneyType {
+    title : string;
+    createdAt : Date;
+    amount : number;
+}
+
+interface HasFormatter{
+    format : () => string
+}
+
+class Payment implements HasFormatter{
+    constructor(private money : MoneyType){}
+    format() : string {
+        return `Spend $${this.money.amount} on ${this.money.title.toUpperCase()} dated ${this.money.createdAt}`
+    }
+}
+
+class Receipt implements HasFormatter {
+    constructor(private money : MoneyType){}
+    format() : string {
+        return `Received $${this.money.amount} for ${this.money.title.toUpperCase()} dated ${this.money.createdAt}`
+    }
+}
+
 window.onload = () => {
     const btnAdd = document.getElementById("btnAdd") as HTMLButtonElement;
     const txtTitle = document.getElementById("txtTitle") as HTMLInputElement;
+    const txtCreatedAt = document.getElementById("created-at") as HTMLInputElement;
+    const selType = document.querySelector("#sel-type") as HTMLSelectElement;
+    const txtAmount = document.getElementById("txtAmount") as HTMLInputElement;
 
     btnAdd.addEventListener("click", (event : Event) => {
         event.preventDefault();
-        console.log("Title : ", txtTitle.value)
+        
+        let money : MoneyType = {
+            amount : parseInt(txtAmount.value),
+            createdAt : new Date(txtCreatedAt.value),
+            title : txtTitle.value
+        }
+
+        let doc : HasFormatter;
+
+        if(selType.value === "payment"){
+            doc = new Payment(money)
+        }else{
+            doc = new Receipt(money)
+        }
+        console.log(doc);
     })
+
 }
