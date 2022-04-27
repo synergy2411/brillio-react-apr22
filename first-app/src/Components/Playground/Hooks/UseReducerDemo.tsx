dd89fbb64e81352099f92d7c30eedc83a0b5850f
Sumit K <synergy2411@outlook.com>
2022-04-27 11:26:47 +0530

    counter state added

--- first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
+++ first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
@@ -4,8 +4,15 @@ interface IState {
     counter : number;
 }
 
+enum Actions {
+    INCREMENT,
+    DECREMENT,
+    ADD_COUNTER,
+    SUBTRACT_COUNTER
+}
+
 interface IAction {
-    type : string;
+    type : Actions;
     payload? : number;
 }
 
@@ -14,9 +21,21 @@ const initialState : IState= {
 }
 
 const reducerFn = (state : IState, action : IAction) => {
-    if(action.type === "INCREMENT"){
+    if(action.type === Actions.INCREMENT){
         return {
             counter : state.counter + 1
+        }
+    }else if (action.type === Actions.DECREMENT){
+        return {
+            counter : state.counter - 1
+        }
+    }else if(action.type === Actions.ADD_COUNTER){
+        return {
+            counter : state.counter + action.payload!
+        }
+    }else if(action.type === Actions.SUBTRACT_COUNTER){
+        return {
+            counter : state.counter - action.payload!
         }
     }
     return state;
@@ -30,8 +49,14 @@ const UseReducerDemo = () => {
             <h4>Counter : {state.counter}</h4> 
             <hr />
             <div className="btn-group">
-                <button className="btn btn-primary" onClick={() => dispatch({type : "INCREMENT"})}>
+                <button className="btn btn-primary" onClick={() => dispatch({type : Actions.INCREMENT})}>
                     Increment</button>
+                <button className="btn btn-success" onClick={() => dispatch({type : Actions.DECREMENT})}>
+                    Decrement</button>
+                <button className="btn btn-dark" onClick={() => dispatch({type : Actions.ADD_COUNTER, payload : 10})}>
+                    Add 10</button>
+                <button className="btn btn-warning" onClick={() => dispatch({type : Actions.SUBTRACT_COUNTER, payload : 5})}>
+                    Subtract 5 </button>
             </div>
         </div>
     )
