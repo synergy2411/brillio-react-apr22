5ae7b6594f2484ef4bde915683a36fb7dfb174f2
Sumit K <synergy2411@outlook.com>
2022-04-27 12:21:51 +0530

    result deleted

--- first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
+++ first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
@@ -1,8 +1,9 @@
 import { useReducer } from "react";
+import "./UseReducerDemo.css"
 
 interface IState {
-    counter : number;
-    result : Array<number>;
+    counter: number;
+    result: Array<number>;
 }
 
 enum Actions {
@@ -10,44 +11,51 @@ enum Actions {
     DECREMENT,
     ADD_COUNTER,
     SUBTRACT_COUNTER,
-    STORE_RESULT
+    STORE_RESULT,
+    DELETE_RESULT
 }
 
 interface IAction {
-    type : Actions;
-    payload? : number;
+    type: Actions;
+    payload?: number;
 }
 
-const initialState : IState = {
-    counter : 0,
-    result : []
+const initialState: IState = {
+    counter: 0,
+    result: []
 }
 
-const reducerFn = (state : IState, action : IAction) => {
-    if(action.type === Actions.INCREMENT){
+const reducerFn = (state: IState, action: IAction) => {
+    if (action.type === Actions.INCREMENT) {
         return {
             ...state,           // {counter, result}
-            counter : state.counter + 1
+            counter: state.counter + 1
         }
-    }else if (action.type === Actions.DECREMENT){
+    } else if (action.type === Actions.DECREMENT) {
         return {
             ...state,
-            counter : state.counter - 1
+            counter: state.counter - 1
         }
-    }else if(action.type === Actions.ADD_COUNTER){
+    } else if (action.type === Actions.ADD_COUNTER) {
         return {
             ...state,
-            counter : state.counter + action.payload!
+            counter: state.counter + action.payload!
         }
-    }else if(action.type === Actions.SUBTRACT_COUNTER){
+    } else if (action.type === Actions.SUBTRACT_COUNTER) {
         return {
             ...state,
-            counter : state.counter - action.payload!
+            counter: state.counter - action.payload!
         }
-    }else if(action.type === Actions.STORE_RESULT){
+    } else if (action.type === Actions.STORE_RESULT) {
         return {
             ...state,
-            result : [state.counter, ...state.result]
+            result: [state.counter, ...state.result]
+        }
+    } else if (action.type === Actions.DELETE_RESULT) {
+        const filteredResult = state.result.filter((r,i) => i !== action.payload)
+        return {
+            ...state,
+            result: filteredResult
         }
     }
     return state;
@@ -58,28 +66,33 @@ const UseReducerDemo = () => {
 
     return (
         <div className="text-center">
-            <h4>Counter : {state.counter}</h4> 
+            <h4>Counter : {state.counter}</h4>
             <hr />
             <div className="btn-group">
-                <button className="btn btn-primary" onClick={() => dispatch({type : Actions.INCREMENT})}>
+                <button className="btn btn-primary" onClick={() => dispatch({ type: Actions.INCREMENT })}>
                     Increment</button>
-                <button className="btn btn-success" onClick={() => dispatch({type : Actions.DECREMENT})}>
+                <button className="btn btn-success" onClick={() => dispatch({ type: Actions.DECREMENT })}>
                     Decrement</button>
-                <button className="btn btn-dark" onClick={() => dispatch({type : Actions.ADD_COUNTER, payload : 10})}>
+                <button className="btn btn-dark" onClick={() => dispatch({ type: Actions.ADD_COUNTER, payload: 10 })}>
                     Add 10</button>
-                <button className="btn btn-warning" onClick={() => dispatch({type : Actions.SUBTRACT_COUNTER, payload : 5})}>
+                <button className="btn btn-warning" onClick={() => dispatch({ type: Actions.SUBTRACT_COUNTER, payload: 5 })}>
                     Subtract 5 </button>
             </div>
             <br />
+            <br />
             <div className="row">
                 <div className="col-4 offset-4">
-                    <button className="btn btn-danger" 
-                        onClick={() => dispatch({type : Actions.STORE_RESULT })}>
-                            Store Result
-                            </button>
-                            <br />
+                    <button className="btn btn-danger btn-block"
+                        onClick={() => dispatch({ type: Actions.STORE_RESULT })}>
+                        Store Result
+                    </button>
+                    <br />
+                    <br />
                     <ul className="list-group">
-                        {state.result.map((r,i) => <li key={i} className="list-group-item">{r}</li>)}
+                        {state.result.map((r, i) => <li onClick={() => dispatch({
+                            type : Actions.DELETE_RESULT, 
+                            payload : i
+                            })} key={i} className="list-group-item">{r}</li>)}
                     </ul>
                 </div>
             </div>
