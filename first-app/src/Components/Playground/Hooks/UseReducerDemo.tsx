d2fa5efd3dd6b8b1f93e8e7854b2584541e8abc3
Sumit K <synergy2411@outlook.com>
2022-04-27 12:06:03 +0530

    storing result

--- first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
+++ first-app/src/Components/Playground/Hooks/UseReducerDemo.tsx
@@ -2,13 +2,15 @@ import { useReducer } from "react";
 
 interface IState {
     counter : number;
+    result : Array<number>;
 }
 
 enum Actions {
     INCREMENT,
     DECREMENT,
     ADD_COUNTER,
-    SUBTRACT_COUNTER
+    SUBTRACT_COUNTER,
+    STORE_RESULT
 }
 
 interface IAction {
@@ -16,26 +18,36 @@ interface IAction {
     payload? : number;
 }
 
-const initialState : IState= {
-    counter : 0
+const initialState : IState = {
+    counter : 0,
+    result : []
 }
 
 const reducerFn = (state : IState, action : IAction) => {
     if(action.type === Actions.INCREMENT){
         return {
+            ...state,           // {counter, result}
             counter : state.counter + 1
         }
     }else if (action.type === Actions.DECREMENT){
         return {
+            ...state,
             counter : state.counter - 1
         }
     }else if(action.type === Actions.ADD_COUNTER){
         return {
+            ...state,
             counter : state.counter + action.payload!
         }
     }else if(action.type === Actions.SUBTRACT_COUNTER){
         return {
+            ...state,
             counter : state.counter - action.payload!
+        }
+    }else if(action.type === Actions.STORE_RESULT){
+        return {
+            ...state,
+            result : [state.counter, ...state.result]
         }
     }
     return state;
@@ -57,6 +69,19 @@ const UseReducerDemo = () => {
                     Add 10</button>
                 <button className="btn btn-warning" onClick={() => dispatch({type : Actions.SUBTRACT_COUNTER, payload : 5})}>
                     Subtract 5 </button>
+            </div>
+            <br />
+            <div className="row">
+                <div className="col-4 offset-4">
+                    <button className="btn btn-danger" 
+                        onClick={() => dispatch({type : Actions.STORE_RESULT })}>
+                            Store Result
+                            </button>
+                            <br />
+                    <ul className="list-group">
+                        {state.result.map((r,i) => <li key={i} className="list-group-item">{r}</li>)}
+                    </ul>
+                </div>
             </div>
         </div>
     )
